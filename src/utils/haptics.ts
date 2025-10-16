26c9ff18840ea010d803d575c41841e488937eda
Roitium <65794453+roitium@users.noreply.github.com>
2025-10-16 21:02:52 +0800

    fix: fire and forget

--- src/utils/haptics.ts
+++ src/utils/haptics.ts
@@ -1,29 +1,22 @@
 import * as ExpoHaptics from 'expo-haptics'
-import { errAsync, ResultAsync } from 'neverthrow'
 import { reportErrorToSentry } from './log'
 
 let hapticsSupported = true
 
-export const performAndroidHapticsAsync = (type: ExpoHaptics.AndroidHaptics) =>
-	ResultAsync.fromPromise(
-		(async () => {
-			if (!hapticsSupported) return
-			try {
-				await ExpoHaptics.performAndroidHapticsAsync(type)
-			} catch (e) {
-				if (e instanceof Error && e.message.includes('is not available')) {
-					// 这用户的手机有点老
-					hapticsSupported = false
-					return
-				}
-				reportErrorToSentry(
-					e,
-					'performAndroidHapticsAsync 出错',
-					'Utils.Haptics',
-				)
-			}
-		})(),
-		(e) => errAsync(e),
-	)
+export const performAndroidHapticsAsync = async (
+	type: ExpoHaptics.AndroidHaptics,
+): Promise<void> => {
+	if (!hapticsSupported) return
+
+	try {
+		await ExpoHaptics.performAndroidHapticsAsync(type)
+	} catch (e) {
+		if (e instanceof Error && e.message.includes('is not available')) {
+			hapticsSupported = false
+			return
+		}
+		reportErrorToSentry(e, 'performAndroidHapticsAsync 出错', 'Utils.Haptics')
+	}
+}
 
 export const AndroidHaptics = ExpoHaptics.AndroidHaptics
