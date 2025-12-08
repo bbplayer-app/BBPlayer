668da697cc6b128a14fce10a46bf1c8908c6fbf0
Roitium <65794453+roitium@users.noreply.github.com>
2025-12-08 22:28:13 +0800

    chore: 1

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import { Orpheus } from '@roitium/expo-orpheus'
+import log from './src/utils/log'
 import { finalizeAndRecordCurrentTrack } from './src/utils/player'
+import toast from './src/utils/toast'
+
 // 定义一个全局变量，避免二次初始化 player
 global.playerIsReady = false
 
 Orpheus.addListener('onPlayerError', (error) => {
-	logger.error('播放器错误事件：', { error })
+	log.error('播放器错误事件：', { error })
 	toast.error(`播放器发生错误: ${error.message || '未知错误'}`, {
 		description: error.code,
 	})
