09bd1ae6cb5e6415daf4067c60d9206bcabe08f4
roitium <65794453+roitium@users.noreply.github.com>
2026-01-26 20:52:42 +0800

    chore: add `@typescript-eslint/no-unsafe-argument` to disabled ESLint rules in `app.config.ts`

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-argument */
 import { execSync } from 'child_process'
 import type { ConfigContext, ExpoConfig } from 'expo/config'
 import { version } from './package.json'
