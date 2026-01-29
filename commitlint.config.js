fd11b15494cf158c9c5d4be811eb33374e41b378
roitium <65794453+roitium@users.noreply.github.com>
2026-01-29 13:19:35 +0800

    feat: 1

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,13 +1,11 @@
 module.exports = {
 	extends: ['@commitlint/config-conventional'],
 	rules: {
-		// scope 必须是以下之一
 		'scope-enum': [
-			2, // 错误级别：2=error
-			'always', // 必须匹配
-			['app', 'docs', 'eitc', 'orpheus', 'root'], // 允许的 scope
+			2,
+			'always',
+			['app', 'docs', 'image-colors', 'orpheus', 'root'],
 		],
-		// 允许空 scope（可选，如果需要强制写 scope，改成 2）
 		'scope-empty': [0, 'never'],
 	},
 }
