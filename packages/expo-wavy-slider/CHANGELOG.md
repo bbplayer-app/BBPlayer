1796deababebb65566f19c4a209177cde1d0d5bb
roitium <65794453+roitium@users.noreply.github.com>
2026-05-25 12:00:51 +0800

    chore(mobile): vendor native tab and slider packages

---
+++ packages/expo-wavy-slider/CHANGELOG.md
@@ -0,0 +1,46 @@
+## [0.4.6](https://github.com/roitium/expo-wavy-slider/compare/v0.4.5...v0.4.6) (2026-05-24)
+
+### Performance Improvements
+
+- move branding to native layer ([7ec2f41](https://github.com/roitium/expo-wavy-slider/commit/7ec2f41e78d25f672b05d6e819a00f45432a7ae6))
+
+## [0.4.5](https://github.com/roitium/expo-wavy-slider/compare/v0.4.4...v0.4.5) (2026-05-24)
+
+### Bug Fixes
+
+- measure before ComposerView attached ([62e7b10](https://github.com/roitium/expo-wavy-slider/commit/62e7b10716f1cfb20b40abf756c5262a05e2cd73))
+
+## [0.4.4](https://github.com/roitium/expo-wavy-slider/compare/v0.4.3...v0.4.4) (2026-05-22)
+
+### Bug Fixes
+
+- measure before ComposerView attached ([bad9463](https://github.com/roitium/expo-wavy-slider/commit/bad946398e0bafa5040df95e700f91436d4ef439))
+
+## [0.4.3](https://github.com/roitium/expo-wavy-slider/compare/v0.4.2...v0.4.3) (2026-05-22)
+
+### Bug Fixes
+
+- SharedObject with id xxx not found in the registry ([d7fb1ce](https://github.com/roitium/expo-wavy-slider/commit/d7fb1cee4a1f303954fb3918a795d62d2dc96d3d))
+
+## [0.4.2](https://github.com/roitium/expo-wavy-slider/compare/v0.4.1...v0.4.2) (2026-05-22)
+
+### Bug Fixes
+
+- worklets serializer racing with expo-ui ([5555096](https://github.com/roitium/expo-wavy-slider/commit/5555096190a29ab43f43eb545c3b6d3cb32051d5))
+
+## [0.4.1](https://github.com/roitium/expo-wavy-slider/compare/v0.4.0...v0.4.1) (2026-05-22)
+
+### Bug Fixes
+
+- race condition ([b68dd5f](https://github.com/roitium/expo-wavy-slider/commit/b68dd5fc844a3e73cfef1195acf80b67087bbb84))
+
+# [0.4.0](https://github.com/roitium/expo-wavy-slider/compare/v0.3.0...v0.4.0) (2026-05-22)
+
+### Bug Fixes
+
+- add rootDir to tsconfig ([ca61321](https://github.com/roitium/expo-wavy-slider/commit/ca6132188779af612943df85c6f9b5c148ebfeaa))
+- ci ([75fb16c](https://github.com/roitium/expo-wavy-slider/commit/75fb16c3dca4bd67a99725eec1bca0c7c732f69e))
+
+### Features
+
+- add thumbShape switch ([87e42b1](https://github.com/roitium/expo-wavy-slider/commit/87e42b125d49fd66e7dddb6ec9d4c8fabfc198ab))
---
+++ packages/expo-wavy-slider/src/worklets.fx.ts
@@ -0,0 +1,88 @@
+/*
+  huge thx to expo-ui
+  inspired by https://github.com/expo/expo/blob/main/packages/expo-ui/src/State/index.fx.ts
+*/
+import { installOnUIRuntime } from 'expo'
+
+import { worklets } from './utils/ensureWorklets'
+import {
+	EXPO_SHARED_OBJECT_ID_KEY,
+	isWavySliderSharedObject,
+	type WavySliderSharedObject,
+} from './utils/sharedObjectBrand'
+
+type PackedSharedObject = { objectId: number }
+
+let serializerRegistered = false
+
+function registerSharedObjectSerializer(): void {
+	if (serializerRegistered || !worklets) return
+
+	serializerRegistered = true
+	worklets.registerCustomSerializable<
+		WavySliderSharedObject,
+		PackedSharedObject
+	>({
+		name: 'ExpoWavySliderSharedObject',
+		determine: (value): value is WavySliderSharedObject => {
+			'worklet'
+			return isWavySliderSharedObject(value)
+		},
+		pack: (value) => {
+			'worklet'
+			return { objectId: value[EXPO_SHARED_OBJECT_ID_KEY] }
+		},
+		unpack: (packed) => {
+			'worklet'
+			const sharedObject = (
+				globalThis as typeof globalThis & {
+					expo: {
+						SharedObject: {
+							__resolveInWorklet(id: number): {
+								getValue?: () => unknown
+								setValue?: (value: { value: unknown }) => void
+							}
+						}
+					}
+				}
+			).expo.SharedObject
+			const obj = sharedObject.__resolveInWorklet(packed.objectId)
+
+			if (
+				typeof obj.getValue === 'function' &&
+				typeof obj.setValue === 'function'
+			) {
+				Object.defineProperty(obj, 'value', {
+					get() {
+						return obj.getValue?.()
+					},
+					set(value: unknown) {
+						if (value === undefined) return
+
+						obj.setValue?.({ value })
+					},
+				})
+			}
+
+			return obj as unknown as WavySliderSharedObject
+		},
+	})
+}
+
+/*
+  `installOnUIRuntime()` currently depends on the runtime pointer exposed via
+  `_WORKLET_RUNTIME`. In practice that value is installed by Reanimated's
+  runtime decorator, so this package requires Reanimated even though the direct
+  worklet APIs come from `react-native-worklets`.
+  https://github.com/software-mansion/react-native-reanimated/blob/cfb12d0f747ad189d2ff7717b0774de8ec6f9897/packages/react-native-reanimated/Common/cpp/reanimated/RuntimeDecorators/RNRuntimeDecorator.cpp#L20
+*/
+try {
+	// oxlint-disable-next-line
+	require('react-native-reanimated')
+} catch {
+	throw new Error(
+		'[expo-wavy-slider] react-native-reanimated is required. Please install it first.',
+	)
+}
+installOnUIRuntime()
+registerSharedObjectSerializer()
